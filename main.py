7dffcd1f22a33261d3a47fdcbc612e4ec64375bd
RoyNetwork <106408391+RoyNetwork@users.noreply.github.com>
2022-06-03 14:30:20 -0300

    Projeto Sasageyo
    Atividade 1 (soma de números inteiros)

---
+++ main.py
@@ -0,0 +1,9 @@
+__Author__ = 'Alan (Roy)'
+
+print('somando dois números inteiros')
+n1=int(input('escreva um número inteiro: '))
+
+n2=int(input('escreva mais um número inteiro: '))
+
+segs=n1+n2
+print(segs)
